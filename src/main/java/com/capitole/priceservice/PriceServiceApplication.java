080aadc9cfce47f27658f10d6d86d585ef915640
TSOFT\Roberto.Arias <Roberto Arias>
2022-11-25 10:28:26 -0300

    Generacion de POM con todas las dependencias necesarias y application.properties con variables de entorno

---
+++ src/main/java/com/capitole/priceservice/PriceServiceApplication.java
@@ -0,0 +1,22 @@
+package com.capitole.priceservice;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+public class PriceServiceApplication extends SpringBootServletInitializer implements ApplicationRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceServiceApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+	}
+
+}
