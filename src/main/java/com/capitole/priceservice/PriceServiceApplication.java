9cf46bc91700abc90aa652bb15d62188e600a302
TSOFT\Roberto.Arias <Roberto Arias>
2022-11-30 10:12:28 -0300

    Se agrega anotacion de ComponentScan a clase principal del servicio

--- src/main/java/com/capitole/priceservice/PriceServiceApplication.java
+++ src/main/java/com/capitole/priceservice/PriceServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan
 public class PriceServiceApplication extends SpringBootServletInitializer implements ApplicationRunner{
 
 	public static void main(String[] args) {
